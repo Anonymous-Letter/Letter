165e2ab69f2abe5b7628689c57d9e109860fada4
yasminyin <yasminyin96@gmail.com>
2021-04-09 16:39:36 -0700

    Add Parse Create Bottom Navigation Create Fragment

--- app/src/main/java/com/example/letter/fragments/HomeFragment.java
+++ app/src/main/java/com/example/letter/fragments/HomeFragment.java
@@ -1,7 +1,6 @@
 package com.example.letter.fragments;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,25 +8,18 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.letter.R;
-import com.parse.ParseException;
-import com.parse.ParseQuery;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class HomeFragment extends Fragment {
 
-    public static final String TAG = "PostsFragment";
+    public static final String TAG = "HomeFragment";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_posts, container, false);
+        return inflater.inflate(R.layout.fragment_home, container, false);
     }
 
     @Override
