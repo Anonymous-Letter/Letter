e9b170a8678b06e3199c34e2fee485e92af4a6aa
yasminyin <yasminyin96@gmail.com>
2021-04-14 22:17:42 -0700

    profile page setup

--- app/src/main/java/com/example/letter/MainActivity.java
+++ app/src/main/java/com/example/letter/MainActivity.java
@@ -4,25 +4,39 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.letter.fragments.ComposeFragment;
 import com.example.letter.fragments.HomeFragment;
 import com.example.letter.fragments.ProfileFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "MainActivity";
+    private Button btnLogout;
     private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        btnLogout = findViewById(R.id.btnLogout);
         bottomNavigationView = findViewById(R.id.bottomNavigation);
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ParseUser.logOut();
+                ParseUser currentUser = ParseUser.getCurrentUser(); // this will now be null
+                goLoginActivity();
+            }
+        });
 
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -48,5 +62,12 @@ public class MainActivity extends AppCompatActivity {
 
         // Set default selection
         bottomNavigationView.setSelectedItemId(R.id.action_home);
+    }
+
+    private void goLoginActivity() {
+
+        Intent i = new Intent(this, LoginActivity.class);
+        startActivity(i);
+        finish();
     }
 }
--- app/src/main/java/com/example/letter/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/letter/fragments/ProfileFragment.java
@@ -5,16 +5,21 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.letter.R;
 
 
 public class ProfileFragment extends Fragment {
     public static final String TAG = "ProfileFragment";
+    private Button btnBack;
+    private Button btnSetting;
+    private RecyclerView rvReply;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -26,5 +31,8 @@ public class ProfileFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        btnBack = view.findViewById(R.id.btnBack);
+        btnSetting = view.findViewById(R.id.btnSetting);
+        rvReply = view.findViewById(R.id.rvReply);
     }
 }
